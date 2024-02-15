29e5ce046da3b961f26f7c6104b7aacf1981ce06
usmanshabbir756 <usmanshabbir756@gmail.com>
2024-02-15 15:35:07 +0500

    dynamic link and replace url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link"
+
 export default function Home(){
-    return <h1>Home page</h1>
+    return (
+    <main>
+        <Link href="/about">About</Link>
+        <h1>Home page</h1>
+    </main>
+    )
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,21 @@
-export const metadata = {
-    title: 'Products'
+import { Metadata } from "next"
+import Link from "next/link"
+
+export const metadata : Metadata = {
+    title: {
+        absolute:"Products"
+    }
   }
   
 
 export default function Prducts(){
-    return <h1>Prducts</h1>
+    const productId=1;
+    return (
+    <main>
+        <Link href="/">Home</Link>
+        <h1>products</h1>
+        <p><Link href={`products/${productId}`}>produc 1</Link></p>
+        <p><Link href="products/2" replace>produc 2</Link></p>
+    </main>
+    )
 }
